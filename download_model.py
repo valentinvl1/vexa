71bea1a0420aa8d1888ab2a1d6d1ddb7e4e4db3f
Hamza <untilhamza@gmail.com>
2025-05-18 02:09:19 +0900

    Enhance model configuration in download_model.py by introducing type hints for model_size, device, and compute_type. Update model instantiation to use these variables, improving clarity and flexibility.

--- download_model.py
+++ download_model.py
@@ -1,7 +1,16 @@
 import os
+from typing import Literal
+#The HF_HOME environment variable configures the local storage location for the Hugging Face library. It dictates where the library stores data, including the user's token and the cache for downloaded models and datasets
 os.environ['HF_HOME'] = '.'
 from faster_whisper import WhisperModel
 
-model_size = "medium"
+model_size:Literal["tiny", "base", "small", "medium", "large-v1", "large-v2", "large-v3", "large", "distil-small", "distil-medium", "distil-large"] = "tiny"
+device:Literal["cpu", "cuda"] = "cpu" #"cuda"
+compute_type:Literal["int8", "float16", "default"] = "default" #"float16"
 
-model = WhisperModel(model_size, device="cuda", compute_type="float16")
+model = WhisperModel(model_size, device=device, compute_type=compute_type)
+
+# segments, _ = model.transcribe("audio.mp3", language="en", task="transcribe")
+
+# for segment in segments:
+#     print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
